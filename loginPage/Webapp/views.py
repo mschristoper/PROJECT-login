e9bbabfa1aa7f49137214801cf81d3530261783e
christoper <mschristoper2@gmail.com>
2021-09-08 20:38:04 +0800

    fixing error

--- loginPage/Webapp/views.py
+++ loginPage/Webapp/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth.forms import  UserCreationForm
 from .forms import Customer
+from django.contrib.auth.models import Group
 
 
 # Create your views here.
 def registerPage(request):  
 
     form = Customer()
+    if request.method == 'POST':
+        form = Customer(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name = 'Customer')
+            user.groups.add(group)
+            
 
     context = {form: 'form'}
-    return render(request, 'Webapp/register.html', context)
+    return render(request, 'accounts/register.html', context)
 
 
 def home(request):
     context = {}
-    return render(request, 'Webapp/home.html' ,context)
+    return render(request, 'accounts/home.html' ,context)
